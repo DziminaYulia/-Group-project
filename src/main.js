f04f5cc64f2cff9b3cb49dd0afadc9d1cd7a5cd4
SerediukAngelina <SerediukAnhelina@vinnvk29.ukr.education>
2025-09-19 17:21:05 +0300

    Ангеліна

---
+++ src/main.js
@@ -0,0 +1,14 @@
+let btn_input = document.querySelector(".input")
+let container_regist = document.querySelector(".registration")
+let btn_sumbit = document.querySelector(".sumbit")
+btn_input.addEventListener("click", () => {
+    container_regist.style.display= "block"
+    btn_input.style.display= "none"
+})
+
+btn_sumbit.addEventListener("click", () => {
+    alert("Вам надано знижку 30%")
+    container_regist.style.display= "none"
+    btn_input.style.display= "flex"
+    btn_input.innerHTML = "30%"
+})
