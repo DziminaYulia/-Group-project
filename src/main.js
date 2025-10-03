59267400f40994d0f43e7d8e2109d877134c163a
Julia <dziminalogika@gmail.com>
2025-10-03 21:03:49 +0300

    s

--- src/main.js
+++ src/main.js
@@ -1,18 +1,3 @@
-let btn_input = document.querySelector(".input");
-let container_regist = document.querySelector(".registration");
-let btn_sumbit = document.querySelector(".sumbit");
-btn_input.addEventListener("click", () => {
-  container_regist.style.display = "block";
-  btn_input.style.display = "none";
-});
-
-btn_sumbit.addEventListener("click", () => {
-  alert("Вам надано знижку 30%");
-  container_regist.style.display = "none";
-  btn_input.style.display = "flex";
-  btn_input.innerHTML = "30%";
-});
-
 // === БРОНЮВАННЯ ===
 document.addEventListener("DOMContentLoaded", function () {
   const bookButtons = document.querySelectorAll(".book-button");
